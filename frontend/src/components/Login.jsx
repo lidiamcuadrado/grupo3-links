df2cd1dcab2aaa1813d40de631e5f3fb9c2e1ee1
Geronimo <geronimomc.1995@gmail.com>
2023-09-26 16:39:16 +0200

    modificacion de algunos estilos css y de etiquetas

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -15,12 +15,12 @@ export const Login = (props) => {
             <h2>Inicio de sesión</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">Correo electrónico :</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="juan.perez@gmail.com" id="email" name="email" />
                 <label htmlFor="password">Contraseña :</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                 <button className="log" type="submit">Iniciar Sesión</button>
             </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('Register')}>¿No tienes cuenta? Registrate aquí</button>
+            <button className="link-btn" onClick={() => props.onFormSwitch('Register')}>¿No tienes cuenta? Registrate aquí.</button>
         </div>
     )
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -13,17 +13,17 @@ export const Register = (props) => {
     return (
         <div className="auth-form-container">
             <h1>WeShare!</h1>
-            <h2>Register</h2>
-        <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Username</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="User name" />
-            <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button className="log" type="submit">Register</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('Login')}>Already have an account? Login here.</button>
-    </div>
+            <h2>Registro</h2>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Nombre completo :</label>
+                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Juan Perez" />
+                <label htmlFor="email">Correo electrónico :</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="juan.perez@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Contraseña :</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button className="log" type="submit">Registrarme</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('Login')}>¿Ya tienes una cuenta? Iniciá sesión aquí.</button>
+        </div>
     )
 }

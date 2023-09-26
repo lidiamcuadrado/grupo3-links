fdc258d1a7fa404579a38ffcf6ddebfd27b509c4
Geronimo <geronimomc.1995@gmail.com>
2023-09-26 15:11:20 +0200

    creacion favicom y modificación css

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,15 +12,15 @@ export const Login = (props) => {
     return (
         <div className="auth-form-container">
             <h1>WeShare!</h1>
-            <h2>Login</h2>
+            <h2>Inicio de sesión</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
+                <label htmlFor="email">Correo electrónico :</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Contraseña :</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button className="log" type="submit">LogIn</button>
+                <button className="log" type="submit">Iniciar Sesión</button>
             </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('Register')}>Don't have an account? Register here.</button>
+            <button className="link-btn" onClick={() => props.onFormSwitch('Register')}>¿No tienes cuenta? Registrate aquí</button>
         </div>
     )
 }

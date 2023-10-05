bd2be21d5629cf613dafc7b5905af66971830455
Miguel Vispo Álvarez <vispoalvarez@gmail.com>
2023-10-05 13:56:48 +0200

    createNotes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Home from "./pages/home";
-
-
+import NoteCreatePage from "./pages/NoteCreatePage/NoteCreatePage.jsx";
 
 const App = () => {
   return (
     <div className="App">
-        <Header/>
+      <Header />
       <Routes>
         <Route path="/notes" element={<Home />} />
         <Route path="/users/register" element={<RegisterPage />} />
         <Route path="/users/login" element={<LoginPage />} />
+        <Route path="/message" element={<NoteCreatePage />} />
       </Routes>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- frontend/src/forms/RegisterForm.jsx
+++ frontend/src/forms/RegisterForm.jsx
@@ -1,53 +1,80 @@
 // Importamos los prop-types.
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 // Importamos los hooks.
-import { useState } from 'react';
-import { getToken } from '../utils/getToken';
-
-
-
+import { useState } from "react";
+import { getToken } from "../utils/getToken";
 
 const RegisterForm = ({ authRegister, loading }) => {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/users/login`;
+    navigate(path);
+  };
 
-    let navigate = useNavigate(); 
-    const routeChange = () =>{ 
-    let path = `/users/login`; 
-    navigate(path);}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    return (
-        <>
-    <div className="auth-form-container">
-            <h1>WeShare!</h1>
-            <h2>Register</h2>
-        <form className="register-form" onSubmit={(e) => {
-                    e.preventDefault();
-                    authRegister(username, email, password);
-                    const token = getToken()
-                    console.log(token);
-                }
-}>
-            <label htmlFor="name">Username</label>
-            <input value={username} name="name" onChange={(e) => setUsername(e.target.value)} id="name" placeholder="User name" />
-            <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">Password</label>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button className="log" disabled={loading}>Register</button>
+  return (
+    <>
+      <div className="auth-form-container">
+        <h1>WeShare!</h1>
+        <h2>Register</h2>
+        <form
+          className="register-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            authRegister(username, email, password);
+            const token = getToken();
+            console.log(token);
+          }}
+        >
+          <label htmlFor="name">Username</label>
+          <input
+            value={username}
+            name="name"
+            onChange={(e) => setUsername(e.target.value)}
+            id="name"
+            placeholder="User name"
+            autoFocus
+            required
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="youremail@gmail.com"
+            id="email"
+            name="email"
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+            required
+          />
+          <button className="log" disabled={loading}>
+            Register
+          </button>
         </form>
-        <button className="link-btn" onClick={routeChange}>Already have an account? Login here.</button>
-    </div>
-        </>
-    );
+        <button className="link-btn" onClick={routeChange}>
+          Already have an account? Login here.
+        </button>
+      </div>
+    </>
+  );
 };
 
 RegisterForm.propTypes = {
-    authRegister: PropTypes.func.isRequired,
-    loading: PropTypes.bool.isRequired,
+  authRegister: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default RegisterForm;
